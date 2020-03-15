e8bfec701f40b7c31f1ff63ae4a506a72a4d721c
Andrew Judd <awjudd@gmail.com>
2020-03-15 08:13:40 -0400

    Starting to build

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# template-cache
+# Template Cache
+
+Sometimes the data on templates is never, or rarely going to change.  So why re-generate it each and every time?
---
+++ src/Providers/CacheServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Awjudd\TemplateCache\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class CacheServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
